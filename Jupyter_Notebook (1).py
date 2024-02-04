03a787917730b3e22b02b150e4cbcf3d39d9e4ad
Isabella Schiappa Castro <158853077+xizzxy@users.noreply.github.com>
2024-02-04 16:00:12 -0500

    Add files via upload

---
+++ Jupyter_Notebook (1).py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/images/SN_web_lightmode.png" width="300" alt="cognitiveclass.ai logo">
+# </center>
+# 
+
+# #### Add your code below following the instructions given in the course
+# 
+
+# In[ ]:
+
+
+Data Science Tools and Ecosystem
+
+
+# In[ ]:
+
+
+In this notebook, Data Science Tools and Ecosystem are summarized
+
+
+# In[ ]:
+
+
+## Data Science Languages
+
+Some of the popular languages that Data Scientists use are: 
+- Python
+- R
+- SQL
+
+
+# In[ ]:
+
+
+## Data Science Libraries
+Some of the commonly used libraries used by Data Scientists include:
+- Pandas
+- NumPy
+- Scikit-learn
+- TensorFlow
+- PyTorch
+- Matplotlib
+- Seaborn
+
+
+# In[ ]:
+
+
+## Data Science Tools
+
+| Tool       | Description                           |
+|------------|---------------------------------------|
+| Jupyter    | Interactive computing environment     |
+| RStudio    | IDE for R programming                 |
+| Git        | Version control system                |
+| GitHub     | Platform for hosting Git repositories |
+| Anaconda   | Python/R data science platform        |
+| Tableau    | Data visualization software           |
+| IBM Watson | AI and machine learning platform      |
+
+
+# In[ ]:
+
+
+## Arithmetic Expression Examples
+Lets explore some basic arithmetic expressions in Python.
+
+
+# In[ ]:
+
+
+# This is a simple arithmetic expression to multiply then add integers.
+result = (3 * 4) + 5
+print("Result:", result)
+
+
+# In[ ]:
+
+
+# This will convert 200 minutes to hours by dividing by 60.
+minutes = 200
+hours = minutes / 60
+print("200 minutes is equal to", hours, "hours.")
+
+
+# In[ ]:
+
+
+Objectives:
+List popular languages for Data Science.
+Identify commonly used libraries in Data Science.
+Summarize key Data Science tools.
+Introduce basic arithmetic expressions in Python.
+Perform simple arithmetic operations in Python.
+
+
+# In[ ]:
+
+
+## Author
+Isabella Schiappa Castro
+
